d4efc095e8173dc45202e3e27b617576eecfa294
Andreas Hemmingsson <andhem@gmail.com>
2018-01-14 17:24:06 +0100

    added hystrix dashboard

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Some very simple examples of what Hystrix can do and how it behaves in different
 
 http://localhost:8080/swagger-ui.html
 
+### Hystrix Dashboard
+
+http://localhost:8080/hystrix/monitor?stream=http%3A%2F%2Flocalhost%3A8080%2Fhystrix.stream&title=hystrix.stream
+
 ##useful links
 
 https://github.com/Netflix/Hystrix/wiki/Configuration
--- src/main/java/com/example/springboothystrix/SpringBootHystrixApplication.java
+++ src/main/java/com/example/springboothystrix/SpringBootHystrixApplication.java
@@ -3,9 +3,12 @@ package com.example.springboothystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class SpringBootHystrixApplication {
 
 	public static void main(String[] args) {
